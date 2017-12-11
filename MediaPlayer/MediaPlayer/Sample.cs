ab24329d275f4af3eefa457df36c5c2bb2653d90
Anthony Jouvel <anthony.jouvel@epitech.eu>
2017-12-11 14:53:40 +0100

    first push

---
+++ MediaPlayer/MediaPlayer/Sample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leap;
+
+namespace MediaPlayer
+{
+    public class Sample
+    {
+        public static void Main()
+        {
+            Controller controller = new Controller();
+            SampleListener listener = new SampleListener();
+            controller.Connect += listener.OnServiceConnect;
+            controller.Device += listener.OnConnect;
+            controller.FrameReady += listener.OnFrame;
+
+            Console.WriteLine("Press Enter to quit ...");
+            Console.ReadLine();
+
+            //controller.RemoveListner(listener);
+            controller.Dispose();
+        }
+    }
+}
---
+++ MediaPlayer/MediaPlayer/SampleListener.cs
@@ -0,0 +1,47 @@
+﻿using Leap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayer
+{
+    class SampleListener
+    {
+        public void OnServiceConnect(object sender, ConnectionEventArgs args)
+        {
+            Console.WriteLine("Srvice Connected");
+        }
+
+        public void OnConnect(object sender, DeviceEventArgs args)
+        {
+            Console.WriteLine("Connected");
+        }
+
+        public void OnFrame(object sender, FrameEventArgs args)
+        {
+            Frame frame = args.frame;
+
+            //Console.WriteLine("Frame id: {0}, timestam: {1}, finders: {2}",
+            //   frame.Id, frame.Timestamp, frame.Hands.Count);
+
+            foreach(Hand hand in frame.Hands)
+            {
+                if (hand.IsLeft)
+                {
+                    Console.WriteLine("  Hand id: {0}, palm position: {1}, fingers: {2}", hand.Id, hand.PalmPosition, hand.Fingers.Count);
+                    Vector normal = hand.PalmNormal;
+                    Vector direction = hand.Direction;
+
+                    Console.WriteLine("  Hand pitch: {0} degrees, roll: {1} degrees, yaw: {2} degrees",
+                        direction.Pitch * 180.0f / (float)Math.PI, normal.Roll * 180.0f / (float)Math.PI, direction.Yaw * 180.0f / (float)Math.PI);
+                }
+                else if (hand.IsRight)
+                {
+                    Console.WriteLine("droite");
+                }
+            }
+        }
+    }
+}
