a4f985f0e3b4aefa237b25d6a3edd323a3a8ce5c
Anthony Jouvel <anthony.jouvel@epitech.eu>
2018-01-04 18:26:49 +0100

    new moves

--- MediaPlayer/MediaPlayer/SampleListener.cs
+++ MediaPlayer/MediaPlayer/SampleListener.cs
@@ -22,26 +22,70 @@ namespace MediaPlayer
         public void OnFrame(object sender, FrameEventArgs args)
         {
             Frame frame = args.frame;
+            
+            //Console.WriteLine("frame id: {0}, timestam: {1}, finders: {2}", frame.Id, frame.Timestamp, frame.Hands.Count);               
 
-            //Console.WriteLine("Frame id: {0}, timestam: {1}, finders: {2}",
-            //   frame.Id, frame.Timestamp, frame.Hands.Count);
+            int extendedfingers = 0;
+            bool pause = false;
 
-            foreach(Hand hand in frame.Hands)
-            {
+            foreach (Hand hand in frame.Hands)
+            {               
+            
                 if (hand.IsLeft)
-                {
-                    Console.WriteLine("  Hand id: {0}, palm position: {1}, fingers: {2}", hand.Id, hand.PalmPosition, hand.Fingers.Count);
-                    Vector normal = hand.PalmNormal;
-                    Vector direction = hand.Direction;
+                {                   
+                    for (int f = 0; f < hand.Fingers.Count; ++f)
+                    {
+                        Finger digit = hand.Fingers[f];
+                        if (digit.IsExtended)
+                            extendedfingers++;
+                    }
+                    if (extendedfingers == 0)
+                        pause = true;
 
-                    Console.WriteLine("  Hand pitch: {0} degrees, roll: {1} degrees, yaw: {2} degrees",
-                        direction.Pitch * 180.0f / (float)Math.PI, normal.Roll * 180.0f / (float)Math.PI, direction.Yaw * 180.0f / (float)Math.PI);
+                    if (hand.Fingers[4].IsExtended && hand.Fingers[1].IsExtended
+                        && hand.Fingers[2].IsExtended == false && hand.Fingers[0].IsExtended == false)
+                        Console.WriteLine("spiderman");
+
+                    if (hand.PinchStrength > 0.9)
+                        Console.WriteLine("pinch gauche");
+                    
+                    //Console.WriteLine(hand.PalmWidth);
+
+                    //Console.WriteLine("  Hand id: {0}, palm position: {1}, fingers: {2}", hand.Id, hand.PalmPosition, hand.Fingers.Count);
+                    //Vector normal = hand.PalmNormal;
+                    //Vector direction = hand.Direction;
+
+                    //Console.WriteLine("  Hand pitch: {0} degrees, roll: {1} degrees, yaw: {2} degrees",
+                    //    direction.Pitch * 180.0f / (float)Math.PI, normal.Roll * 180.0f / (float)Math.PI, direction.Yaw * 180.0f / (float)Math.PI);
+
                 }
                 else if (hand.IsRight)
                 {
-                    Console.WriteLine("droite");
-                }
+                    for (int f = 0; f < hand.Fingers.Count; ++f)
+                    {
+                        Finger digit = hand.Fingers[f];
+                        if (digit.IsExtended)
+                            extendedfingers++;
+                    }
+                    if (extendedfingers == 0)
+                        pause = true;
+
+                    if (hand.PinchStrength > 0.9)
+                        Console.WriteLine("pinch droite");
+                    //Console.WriteLine("right : " + extendedfingers);
+
+                    //Console.WriteLine(hand.PinchDistance);
+                    //console.writeline(hand.pinchstrength);
+
+                }                
             }
+
+                
+            //if (pause == true && extendedfingers == 0)
+            //    Console.WriteLine("Pause");
+            //else
+            //    Console.WriteLine("Play");
+    
         }
     }
 }
